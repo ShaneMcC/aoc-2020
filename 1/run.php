e9162dc32f590c298dc9e92b932cbd257bb36291
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-01 19:38:58 +0000

    Break if we go above the target number.

--- 1/run.php
+++ 1/run.php
@@ -7,23 +7,33 @@
 	function part1($input) {
 		for ($i = 0; $i < count($input); $i++) {
 			for ($j = $i + 1; $j < count($input); $j++) {
-				if ($input[$i] + $input[$j] == 2020) {
+				$sum = $input[$i] + $input[$j];
+				if ($sum == 2020) {
 					return [$input[$i], $input[$j]];
+				} else if ($sum > 2020) {
+					break;
 				}
 			}
 		}
+
+		return [0, 0];
 	}
 
 	function part2($input) {
 		for ($i = 0; $i < count($input); $i++) {
 			for ($j = $i + 1; $j < count($input); $j++) {
 				for ($k = $j + 1; $k < count($input); $k++) {
-					if ($input[$i] + $input[$j] + $input[$k] == 2020) {
+					$sum = $input[$i] + $input[$j] + $input[$k];
+					if ($sum == 2020) {
 						return [$input[$i], $input[$j], $input[$k]];
+					} else if ($sum > 2020) {
+						break;
 					}
 				}
 			}
 		}
+
+		return [0, 0, 0];
 	}
 
 	$part1 = part1($input);
