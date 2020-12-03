88164c04e29f2117e4f34bc054f63bc0d93eedf4
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-03 05:17:51 +0000

    Tidy up a bit.

--- 3/run.php
+++ 3/run.php
@@ -1,32 +1,18 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
-	$input = getInputLines();
-
-	$map = [];
-	foreach ($input as $line) {
-		$map[] = str_split($line);
-	}
-
-	function getTile($map, $x, $y) {
-		$line = isset($map[$y]) ? $map[$y] : ['.'];
-
-		return $line[$x % count($line)];
-	}
+	$map = getInputMap();
 
 	function getSlope($map, $left = 3, $down = 1) {
-		$y = 0;
-		$x = 0;
-		$count = 0;
+		$x = $y = $count = 0;
 
-		while ($y < count($map)) {
+		do {
+			$tile = $map[$y][$x % count($map[$y])];
+			if ($tile == '#') { $count++; }
+
 			$x += $left;
 			$y += $down;
-
-			$tile = getTile($map, $x, $y);
-
-			if ($tile == '#') { $count++; }
-		}
+		} while ($y < count($map));
 
 		return $count;
 	}
