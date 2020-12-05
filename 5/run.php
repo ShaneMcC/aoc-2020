8b5ad4b98ad24654faf007635bde45e16384689c
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-05 05:18:53 +0000

    Tidy up a bit.

--- 5/run.php
+++ 5/run.php
@@ -1,49 +1,39 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
-	$input = getInputLines();
 
 	$part1 = 0;
 
-	$entries = [];
-	foreach ($input as $line) {
-		$bits = str_split($line);
+	$seats = [];
+	foreach (getInputLines() as $line) {
+		$minCol = $minRow = 0;
+		$maxRow = 128;
+		$maxCol = 8;
 
-		$RowMin = 0;
-		$RowMax = 128;
-
-		$ColMin = 0;
-		$ColMax = 8;
-
-		foreach ($bits as $bit) {
+		foreach (str_split($line) as $bit) {
 			if ($bit == 'F') {
-				$RowMax -= ($RowMax - $RowMin) / 2;
+				$maxRow -= ($maxRow - $minRow) / 2;
 			} else if ($bit == 'B') {
-				$RowMin += ($RowMax - $RowMin) / 2;
+				$minRow += ($maxRow - $minRow) / 2;
 			} else if ($bit == 'L') {
-				$ColMax -= ($ColMax - $ColMin) / 2;
+				$maxCol -= ($maxCol - $minCol) / 2;
 			} else if ($bit == 'R') {
-				$ColMin += ($ColMax - $ColMin) / 2;
+				$minCol += ($maxCol - $minCol) / 2;
 			}
 		}
 
-		$seatId = $RowMin * 8 + $ColMin;
-
-		$entries[$seatId] = ['data' => $line, 'row' => $RowMin, 'col' => $ColMin, 'seatId' => $seatId];
-
+		$seatId = ($minRow * 8) + $minCol;
+		$seats[] = $seatId;
 		$part1 = max($seatId, $part1);
 	}
-
-	$seats = array_keys($entries);
 	sort($seats);
 
-	$part2 = 0;
+	$part2 = array_shift($seats);
+
 	foreach ($seats as $s) {
-		if ($part2 == 0) { $part2 = $s; continue; }
 		if ($part2 + 1 != $s) { $part2 = $part2 + 1; break; }
 		$part2 = $s;
 	}
-
 
 	echo 'Part 1: ', $part1, "\n";
 	echo 'Part 2: ', $part2, "\n";
