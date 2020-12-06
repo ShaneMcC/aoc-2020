04dde82d4a703781d23d3ae17797baabed653949
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-06 05:11:04 +0000

    Day 6. Part 2. 714.

--- 6/run.php
+++ 6/run.php
@@ -3,22 +3,33 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 
 	$answers = [];
-	$answer = [];
+	$answer = ['_' => 0];
 	foreach (explode("\n", getInputContent()) as $line) {
 		if (empty($line)) {
 			if (count($answer) > 0) { $answers[] = $answer; }
-			$answer = [];
+			$answer = ['_' => 0];
 		} else {
+			$answer['_']++;
+
 			foreach (str_split($line) as $bit) {
-				$answer[$bit] = true;
+				if (!isset($answer[$bit])) { $answer[$bit] = 0; }
+				$answer[$bit]++;
 			}
 		}
 	}
 	if (count($answer) > 0) { $answers[] = $answer; }
 
-	$part1 = 0;
+	$part2 = $part1 = 0;
 	foreach ($answers as $group) {
-		$part1 += count(array_keys($group));
+		$part1 += count(array_keys($group)) - 1;
+
+		foreach ($group as $g => $c) {
+			if ($g == '_') { continue; }
+			if ($c == $group['_']) {
+				$part2++;
+			}
+		}
 	}
 
-	echo 'Part 1:', $part1, "\n";
+	echo 'Part 1: ', $part1, "\n";
+	echo 'Part 2: ', $part2, "\n";
