b26eb776c14e260aa49442390228ee8e373fa6aa
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-08 06:00:48 +0000

    More tidying.

--- 8/run.php
+++ 8/run.php
@@ -3,59 +3,50 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	require_once(dirname(__FILE__) . '/../common/VM.php');
 	$input = VM::parseInstrLines(getInputLines());
-	$originalInput = $input;
 
 	class Day8VM extends VM {
+		// Accumulator
+		protected $accumulator = 0;
+		public function getAccumulator() { return $this->accumulator; }
+		public function setAccumulator($value) { $this->accumulator = $value; }
+
 		/**
 		 * Init the opcodes.
 		 */
 		protected function init() {
 			/**
-			 * ACC <val>
-			 *
-			 * Increases or decreases a single global value called the
-			 * accumulator by the value given in the argument.
+			 * ACC <val> - Increases or decreases a single global accumulator.
 			 */
 			$this->instrs['acc'] = function($vm, $args) {
 				return $this->setAccumulator($this->getAccumulator() + $args[0]);
 			};
 
 			/**
-			 * JMP <val>
-			 *
-			 * Jumps to a new instruction relative to itself.
+			 * JMP <val> - Jumps to a new instruction relative to itself.
 			 */
 			$this->instrs['jmp'] = function($vm, $args) {
 				return $vm->jump($vm->getLocation() + $args[0]);
 			};
 
 			/**
-			 * NOP
-			 *
-			 * Do nothing, ignores any arguments.
+			 * NOP -Do nothing, ignores any arguments.
 			 */
 			$this->instrs['nop'] = function($vm, $args) {
 				return;
 			};
 		}
-
-		// Accumulator
-		protected $accumulator = 0;
-		public function getAccumulator() { return $this->accumulator; }
-		public function setAccumulator($value) { $this->accumulator = $value; }
 	}
 
-	function codeHasExit($input, $loopValue = 10) {
+	function codeHasExit($input) {
 		$vm = new Day8VM($input);
 		$vm->setDebug(isDebug());
 
 		$visited = [];
 		while (true) {
-			if (!isset($visited[$vm->getLocation()])) { $visited[$vm->getLocation()] = 0; }
-			$visited[$vm->getLocation()]++;
+			$visited[$vm->getLocation()] = true;
 			if (!$vm->step()) { break; }
 
-			if (isset($visited[$vm->getLocation()]) && $visited[$vm->getLocation()] >= $loopValue) {
+			if (isset($visited[$vm->getLocation()])) {
 				return [FALSE, $vm->getAccumulator()];
 			}
 		}
@@ -67,18 +58,19 @@
 	echo 'Part 1: ', $part1[1], "\n";
 
 	for ($i = 0; $i < count($input); $i++) {
-		$input = $originalInput;
-		$line = $input[$i];
-
-		if ($line[0] == "acc") {
+		$checkCode = $input[$i][0];
+		if ($checkCode == "acc") {
 			continue;
-		} else if ($line[0] == "nop") {
+		} else if ($checkCode == "nop") {
 			$input[$i][0] = 'jmp';
-		} else if ($line[0] == "jmp") {
+		} else if ($checkCode == "jmp") {
 			$input[$i][0] = 'nop';
 		}
-
 		$part2 = codeHasExit($input, 1);
+
+		// Reset our change.
+		$input[$i][0] = $checkCode;
+
 		if ($part2[0] !== FALSE) {
 			echo 'Part 2: ', $part2[1], "\n";
 			break;
