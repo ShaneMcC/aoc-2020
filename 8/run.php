ee5e9ad4fc10c6207ddebd8967b3f2042ff0d7a0
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-08 05:35:20 +0000

    Day 8. Part 2. 2974.

--- 8/run.php
+++ 8/run.php
@@ -18,5 +18,42 @@
 			break;
 		}
 	}
-	$vm->reset();
 
+	function testCode($input) {
+		$vm = new Day8VM($input);
+		$vm->setDebug(isDebug());
+
+		$visited = [];
+		while (true) {
+			if (!isset($visited[$vm->getLocation()])) { $visited[$vm->getLocation()] = 0; }
+			$visited[$vm->getLocation()]++;
+			if (!$vm->step()) { break; }
+
+			if (isset($visited[$vm->getLocation()]) && $visited[$vm->getLocation()] >= 100) {
+				return FALSE;
+			}
+		}
+
+		return $vm->accum;
+	}
+
+
+	$originalInput = $input;
+	for ($i = 0; $i < count($input); $i++) {
+		$input = $originalInput;
+		$line = $input[$i];
+
+		if (startsWith($line, "acc")) { continue; }
+
+		if (startsWith($line, "nop")) {
+			$input[$i] = str_replace('jmp', 'jmp', $line);
+		} else if (startsWith($line, "jmp")) {
+			$input[$i] = str_replace('jmp', 'nop', $line);
+		}
+
+		$res = testCode($input);
+		if ($res !== FALSE) {
+			echo 'Part 2: ', $res, "\n";
+			break;
+		}
+	}
