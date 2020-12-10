558c064c60d63f860221129c9effe3248af692b6
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-10 05:25:11 +0000

    Day 10. Part 2. 904.

--- 10/run.php
+++ 10/run.php
@@ -3,26 +3,6 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	/* $current = 0;
-	$chain = [];
-	while (count($chain) < count($input)) {
-		$next1 = array_search($current + 1, $input);
-		$next2 = array_search($current + 2, $input);
-		$next3 = array_search($current + 3, $input);
-
-		if ($next1 !== FALSE) {
-			$chain[] = $input[$next1];
-			$current = $input[$next1];
-		} else if ($next2 !== FALSE) {
-			$chain[] = $input[$next2];
-			$current = $input[$next2];
-		} else if ($next3 !== FALSE) {
-			$chain[] = $input[$next3];
-			$current = $input[$next3];
-		}
-	}
-	$foo = $chain; */
-
 	$foo = $input;
 	sort($foo);
 	$prev = $one = $three = 0;
@@ -36,7 +16,47 @@
 	}
 	$three++;
 
-	echo $one, "\n";
-	echo $three, "\n";
 	$part1 = $one * $three;
 	echo 'Part 1: ', $part1, "\n";
+
+	function getOptions($input, $current) {
+		$options = [];
+
+		$next1 = array_search($current + 1, $input);
+		$next2 = array_search($current + 2, $input);
+		$next3 = array_search($current + 3, $input);
+
+		if ($next1 !== FALSE) { $options[] = $input[$next1]; }
+		if ($next2 !== FALSE) { $options[] = $input[$next2]; }
+		if ($next3 !== FALSE) { $options[] = $input[$next3]; }
+
+		return $options;
+	}
+
+	$max = max($input) + 3;
+
+	$valid = 0;
+	$options = [];
+	foreach (getOptions($input, 0) as $o) { $options[$o] = ['count' => 1]; }
+
+	while (!empty($options)) {
+		$k = array_keys($options);
+		$last = array_shift($k);
+		$c = $options[$last]['count'];
+		unset($options[$last]);
+
+		$v = getOptions($input, $last);
+		if (empty($v)) {
+			if ($last + 3 >= $max) {
+				$valid += $c;
+			}
+		} else {
+			foreach ($v as $o) {
+				if (!isset($options[$o])) { $options[$o] = ['count' => 0]; }
+				$options[$o]['count'] += $c;
+			}
+		}
+	}
+
+	$part2 = $valid;
+	echo 'Part 2: ', $part2, "\n";
