6a3b46e05605ecd10ed6bbe0c90c2c75609393f3
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-11 08:06:16 +0000

    Minor attempts at speed improvements.

--- 11/run.php
+++ 11/run.php
@@ -4,23 +4,23 @@
 	$map = getInputMap();
 
 	function step($map, $adjacentCount = 4, $adjacencyFunction = NULL) {
+		if (!is_callable($adjacencyFunction)) { return $map; }
+
 		$newMap = $map;
 		foreach (yieldXY(0, 0, count($map[0]) - 1, count($map) - 1) as $x => $y) {
 			if ($map[$y][$x] == '.') { continue; }
 
 			$adjacent = 0;
-			$seatsToCheck = is_callable($adjacencyFunction) ? call_user_func_array($adjacencyFunction, [$map, $x, $y]) : [];
 
-			foreach ($seatsToCheck as $s) {
-				[$x2, $y2] = $s;
+			foreach (call_user_func_array($adjacencyFunction, [$map, $x, $y]) as $x2 => $y2) {
 				if ($x == $x2 && $y == $y2) { continue; }
 				if (!isset($map[$y2][$x2])) { continue; }
 
 				if ($map[$y2][$x2] == '#') { $adjacent++; }
+				if ($adjacent >= $adjacentCount) { $newMap[$y][$x] = 'L'; break; }
 			}
 
 			if ($adjacent == 0) { $newMap[$y][$x] = '#'; }
-			if ($adjacent >= $adjacentCount) { $newMap[$y][$x] = 'L'; }
 		}
 
 		return $newMap;
@@ -45,20 +45,16 @@
 	}
 
 	$part1 = stepUntilNoChanges($map, 4, function($map, $x, $y) {
-		$adjacent = [];
 		foreach (yieldXY($x - 1, $y - 1, $x + 1, $y + 1) as $x2 => $y2) {
 			if ($x == $x2 && $y == $y2) { continue; }
 			if (!isset($map[$y2][$x2])) { continue; }
 
-			$adjacent[] = [$x2, $y2];
+			yield $x2 => $y2;
 		}
-		return $adjacent;
 	});
 	echo 'Part 1: ', $part1, "\n";
 
 	$part2 = stepUntilNoChanges($map, 5, function($map, $x, $y) {
-		$visible = [];
-
 		$changes = [[-1, 0],
 		            [0, -1],
 		            [+1, 0],
@@ -76,12 +72,10 @@
 				$y2 += $c[1];
 				if (!isset($map[$y2][$x2])) { break; }
 				if ($map[$y2][$x2] != '.') {
-					$visible[] = [$x2, $y2];
+					yield $x2 => $y2;
 					break;
 				}
 			}
 		}
-
-		return $visible;
 	});
 	echo 'Part 2: ', $part2, "\n";
