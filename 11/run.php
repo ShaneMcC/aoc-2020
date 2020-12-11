52da24c32e68404a77e635828ccbbe98d77f8777
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-11 16:33:49 +0000

    Speed up by removing elements in the map we don't care about.

--- 11/run.php
+++ 11/run.php
@@ -5,14 +5,18 @@
 
 	[$minX, $minY, $maxX, $maxY] = getBoundingBox($map);
 
+	foreach (yieldXY($minX, $minY, $maxX, $maxY) as $x => $y) {
+		if ($map[$y][$x] == '.') {
+			unset($map[$y][$x]);
+		}
+	}
+
 	function step($map, $adjacentCount = 4, $adjacencyFunction = NULL) {
 		if (!is_callable($adjacencyFunction)) { return $map; }
 
 		$newMap = $map;
 		foreach ($map as $y => $row) {
 			foreach ($row as $x => $_) {
-				if ($map[$y][$x] == '.') { continue; }
-
 				$adjacent = 0;
 
 				foreach (call_user_func_array($adjacencyFunction, [$map, $x, $y]) as $x2 => $y2) {
@@ -27,10 +31,12 @@
 		return $newMap;
 	}
 
-	function stepUntilNoChanges($map, $adjacentCount, $useVisibleSeats) {
+	function stepUntilNoChanges($map, $adjacentCount, $adjacencyFunction) {
 		$prev = $map;
+		$count = 0;
 		while (true) {
-			$new = step($prev, $adjacentCount, $useVisibleSeats);
+			$count++;
+			$new = step($prev, $adjacentCount, $adjacencyFunction);
 			if ($new == $prev) { break; }
 			$prev = $new;
 		}
@@ -40,8 +46,7 @@
 			$acv = array_count_values($row);
 			$ans += isset($acv['#']) ? $acv['#'] : 0;
 		}
-
-		return $ans;
+		return [$count, $ans];
 	}
 
 	$part1 = stepUntilNoChanges($map, 4, function($map, $x, $y) {
@@ -52,7 +57,7 @@
 			yield $x2 => $y2;
 		}
 	});
-	echo 'Part 1: ', $part1, "\n";
+	echo 'Part 1: ', $part1[1], ' after ', $part1[0], ' steps.', "\n";
 
 	$part2 = stepUntilNoChanges($map, 5, function($map, $x, $y) use ($minX, $minY, $maxX, $maxY) {
 		$changes = [[-1, 0],
@@ -70,12 +75,10 @@
 			while ($x2 >= $minX && $y2 >= $minY && $x2 <= $maxX && $y2 <= $maxY) {
 				$x2 += $c[0];
 				$y2 += $c[1];
-				if (!isset($map[$y2][$x2])) { break; }
-				if ($map[$y2][$x2] == '#' || $map[$y2][$x2] == 'L') {
-					yield $x2 => $y2;
-					break;
-				}
+				if (!isset($map[$y2][$x2])) { continue; }
+				yield $x2 => $y2;
+				break;
 			}
 		}
 	});
-	echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2[1], ' after ', $part2[0], ' steps.', "\n";
