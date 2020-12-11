50bfbb171ede8d80a7b98cbf6d9e6393780d5e2f
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-11 05:43:36 +0000

    Tidy up a bit.

--- 11/run.php
+++ 11/run.php
@@ -3,143 +3,55 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$map = getInputMap();
 
-	function draw($map) {
-		foreach ($map as $line) {
-			echo implode('', $line), "\n";
+	function getAdjacentSeats($map, $x, $y) {
+		$adjacent = [];
+		foreach (yieldXY($x - 1, $y - 1, $x + 1, $y + 1) as $x2 => $y2) {
+			if ($x == $x2 && $y == $y2) { continue; }
+			if (!isset($map[$y2][$x2])) { continue; }
+
+			$adjacent[] = [$x2, $y2];
 		}
+		return $adjacent;
 	}
-
-	function stepPart1($map) {
-		$newMap = $map;
-		foreach (yieldXY(0, 0, count($map[0]) - 1, count($map) - 1) as $x => $y) {
-			if ($map[$y][$x] == '.') { continue; }
-
-			$adjacent = 0;
-			foreach (yieldXY($x - 1, $y - 1, $x + 1, $y + 1) as $x2 => $y2) {
-				if ($x == $x2 && $y == $y2) { continue; }
-				if (!isset($map[$y2][$x2])) { continue; }
-
-				if ($map[$y2][$x2] == '#') { $adjacent++; }
-			}
-
-			if ($adjacent == 0) { $newMap[$y][$x] = '#'; }
-			if ($adjacent >= 4) { $newMap[$y][$x] = 'L'; }
-		}
-
-		return $newMap;
-	}
-
-	$prev = $map;
-	$part1 = 0;
-	while (true) {
-		$new = stepPart1($prev);
-
-		if ($new == $prev) { break; }
-		$prev = $new;
-	}
-
-	$part1 = 0;
-	foreach (yieldXY(0, 0, count($prev[0]) - 1, count($prev) - 1) as $x => $y) {
-		if ($prev[$y][$x] == '#') {
-			$part1++;
-		}
-	}
-
-	echo 'Part 1: ', $part1, "\n";
 
 	function getVisibleSeats($map, $x, $y) {
 		$visible = [];
 
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$x2--;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
+		$changes = [[-1, 0],
+		            [0, -1],
+		            [+1, 0],
+		            [0, +1],
+		            [-1, -1],
+		            [-1, +1],
+		            [+1, -1],
+		            [+1, +1]
+		           ];
 
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$x2++;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
-
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$y2--;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
-
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$y2++;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
-
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$x2--; $y2--;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
-
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$x2--; $y2++;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
-
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$x2++; $y2--;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
-			}
-		}
-
-		$x2 = $x; $y2 = $y;
-		while (true) {
-			$x2++; $y2++;
-			if (!isset($map[$y2][$x2])) { break; }
-			if ($map[$y2][$x2] != '.') {
-				$visible[] = [$x2, $y2];
-				break;
+        foreach ($changes as $c) {
+			$x2 = $x; $y2 = $y;
+			while (true) {
+				$x2 += $c[0];
+				$y2 += $c[1];
+				if (!isset($map[$y2][$x2])) { break; }
+				if ($map[$y2][$x2] != '.') {
+					$visible[] = [$x2, $y2];
+					break;
+				}
 			}
 		}
 
 		return $visible;
 	}
 
-	function stepPart2($map) {
+	function step($map, $adjacentCount = 4, $part2 = false) {
 		$newMap = $map;
 		foreach (yieldXY(0, 0, count($map[0]) - 1, count($map) - 1) as $x => $y) {
 			if ($map[$y][$x] == '.') { continue; }
 
 			$adjacent = 0;
-			foreach (getVisibleSeats($map, $x, $y) as $s) {
+			$seatsToCheck = $part2 ? getVisibleSeats($map, $x, $y) : getAdjacentSeats($map, $x, $y);
+
+			foreach ($seatsToCheck as $s) {
 				[$x2, $y2] = $s;
 				if ($x == $x2 && $y == $y2) { continue; }
 				if (!isset($map[$y2][$x2])) { continue; }
@@ -148,26 +60,33 @@
 			}
 
 			if ($adjacent == 0) { $newMap[$y][$x] = '#'; }
-			if ($adjacent >= 5) { $newMap[$y][$x] = 'L'; }
+			if ($adjacent >= $adjacentCount) { $newMap[$y][$x] = 'L'; }
 		}
 
 		return $newMap;
 	}
 
-	$prev = $map;
-	$part2 = 0;
-	while (true) {
-		$new = stepPart2($prev);
+	function stepUntilNoChanges($map, $adjacentCount, $part2) {
+		$prev = $map;
+		while (true) {
+			$new = step($prev, $adjacentCount, $part2);
 
-		if ($new == $prev) { break; }
-		$prev = $new;
-	}
-
-	$part2 = 0;
-	foreach (yieldXY(0, 0, count($prev[0]) - 1, count($prev) - 1) as $x => $y) {
-		if ($prev[$y][$x] == '#') {
-			$part2++;
+			if ($new == $prev) { break; }
+			$prev = $new;
 		}
+
+		$ans = 0;
+		foreach (yieldXY(0, 0, count($prev[0]) - 1, count($prev) - 1) as $x => $y) {
+			if ($prev[$y][$x] == '#') {
+				$ans++;
+			}
+		}
+
+		return $ans;
 	}
 
+	$part1 = stepUntilNoChanges($map, 4, false);
+	echo 'Part 1: ', $part1, "\n";
+
+	$part2 = stepUntilNoChanges($map, 5, true);
 	echo 'Part 2: ', $part2, "\n";
