3e651cbd919db264abef9253967397cfd0ffd73e
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-12 05:29:58 +0000

    Some tidying.

--- 12/run.php
+++ 12/run.php
@@ -10,88 +10,70 @@
 		$entries[] = ['dir' => $dir, 'amount' => $amount];
 	}
 
-	$x = $y = 0;
-
-	$changes = ['N' => [0, -1],
-	            'S' => [0, +1],
-	            'E' => [+1, 0],
-	            'W' => [-1, 0],
+	$changes = ['N' => ['d' => [0, -1], 'L' => 'W', 'R' => 'E'],
+	            'S' => ['d' => [0, +1], 'L' => 'E', 'R' => 'W'],
+	            'E' => ['d' => [+1, 0], 'L' => 'N', 'R' => 'S'],
+	            'W' => ['d' => [-1, 0], 'L' => 'S', 'R' => 'N'],
 	           ];
 
-	$lastDir = 'E';
-	foreach ($entries as $e) {
-		$dir = $e['dir'];
-		$amount = $e['amount'];
+    function getPositionRaw($entries) {
+    	global $changes;
 
-		if ($dir == 'F') {
-			$dir = $lastDir;
-		} else if ($dir == 'L' || $dir == 'R') {
-			$amount = $amount / 90;
-			while ($amount > 0) {
-				if ($lastDir == 'E') {
-					$lastDir = ($dir == 'L') ? 'N' : 'S';
-				} else if ($lastDir == 'N') {
-					$lastDir = ($dir == 'L') ? 'W' : 'E';
-				} else if ($lastDir == 'W') {
-					$lastDir = ($dir == 'L') ? 'S' : 'N';
-				} else if ($lastDir == 'S') {
-					$lastDir = ($dir == 'L') ? 'E' : 'W';
+		$x = $y = 0;
+		$lastDir = 'E';
+		foreach ($entries as $e) {
+			$dir = $e['dir'];
+			$amount = $e['amount'];
+
+			if ($dir == 'F' || $dir == 'N' || $dir == 'S' || $dir == 'E' || $dir == 'W') {
+				if ($dir == 'F') { $dir = $lastDir; }
+
+				$x += $changes[$dir]['d'][0] * $amount;
+				$y += $changes[$dir]['d'][1] * $amount;
+			} else if ($dir == 'L' || $dir == 'R') {
+				for ($a = $amount; $a != 0; $a -= 90) {
+					$lastDir = $changes[$lastDir][$dir];
 				}
-				$amount--;
 			}
-
-			continue;
 		}
 
-
-		if (isset($changes[$dir])) {
-			$x += $changes[$dir][0] * $amount;
-			$y += $changes[$dir][1] * $amount;
-		}
+		return [$x, $y];
 	}
 
-	$part1 = manhattan(0, 0, $x, $y);
+	function getPositionWithWaypoint($entries) {
+		global $changes;
+
+		$x = $y = 0;
+		$wX = 10;
+		$wY = -1;
+		foreach ($entries as $e) {
+			$dir = $e['dir'];
+			$amount = $e['amount'];
+
+			if ($dir == 'F') {
+				$x += $wX * $amount;
+				$y += $wY * $amount;
+			} else if ($dir == 'N' || $dir == 'S' || $dir == 'E' || $dir == 'W') {
+				$wX += $changes[$dir]['d'][0] * $amount;
+				$wY += $changes[$dir]['d'][1] * $amount;
+			} else if ($dir == 'L' || $dir == 'R') {
+				for ($a = $amount; $a != 0; $a -= 90) {
+					$oldWX = $wX;
+					$oldWY = $wY;
+
+					$wX = ($dir == 'L') ? $oldWY : 0 - $oldWY;
+					$wY = ($dir == 'L') ? 0 - $oldWX : $oldWX;
+				}
+			}
+		}
+
+		return [$x, $y];
+	}
+
+	$part1 = getPositionRaw($entries);
+	$part1 = manhattan(0, 0, $part1[0], $part1[1]);
 	echo 'Part 1: ', $part1, "\n";
 
-
-	$x = $y = 0;
-
-	$wX = 10;
-	$wY = -1;
-
-	$lastDir = 'E';
-	foreach ($entries as $e) {
-		$dir = $e['dir'];
-		$amount = $e['amount'];
-		$isWaypoint = true;
-
-		if ($dir == 'F') {
-			$x += $wX * $amount;
-			$y += $wY * $amount;
-			continue;
-		} else if ($dir == 'N' || $dir == 'S' || $dir == 'E' || $dir == 'W') {
-			$wX += $changes[$dir][0] * $amount;
-			$wY += $changes[$dir][1] * $amount;
-		} else if ($dir == 'L' || $dir == 'R') {
-			$amount = $amount / 90;
-			while ($amount > 0) {
-				$oldWX = $wX;
-				$oldWY = $wY;
-
-				$isLeft = ($dir == 'L');
-				if ($isLeft) {
-					$wX = $oldWY;
-					$wY = 0 - $oldWX;
-				} else {
-					$wX = 0 - $oldWY;
-					$wY = $oldWX;
-				}
-				$amount--;
-			}
-
-			continue;
-		}
-	}
-
-	$part2 = manhattan(0, 0, $x, $y);
+	$part2 = getPositionWithWaypoint($entries);
+	$part2 = manhattan(0, 0, $part2[0], $part2[1]);
 	echo 'Part 2: ', $part2, "\n";
