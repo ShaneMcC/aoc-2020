0db6ba09dd228b80185121bcd17fb0a08e61eaac
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-12 01:02:29 +0000

    Speed up with some caching.

--- 11/run.php
+++ 11/run.php
@@ -19,9 +19,13 @@
 			foreach ($row as $x => $_) {
 				$adjacent = 0;
 
-				foreach (call_user_func_array($adjacencyFunction, [$map, $x, $y]) as $x2 => $y2) {
-					if ($map[$y2][$x2] == '#') { $adjacent++; }
-					if ($adjacent >= $adjacentCount) { $newMap[$y][$x] = 'L'; break; }
+				$cells = call_user_func_array($adjacencyFunction, [$map, $x, $y]);
+				if (!empty($cells)) {
+					foreach ($cells as $cell) {
+						[$x2, $y2] = $cell;
+						if ($map[$y2][$x2] == '#') { $adjacent++; }
+						if ($adjacent >= $adjacentCount) { $newMap[$y][$x] = 'L'; break; }
+					}
 				}
 
 				if ($adjacent == 0) { $newMap[$y][$x] = '#'; }
@@ -49,17 +53,30 @@
 		return [$count, $ans];
 	}
 
-	$part1 = stepUntilNoChanges($map, 4, function($map, $x, $y) {
+	$part1Cache = [];
+	$part1 = stepUntilNoChanges($map, 4, function($map, $x, $y) use (&$part1Cache) {
+		$cellId = $y . '.' . $x;
+		if (isset($part1Cache[$cellId])) {
+			return $part1Cache[$cellId];
+		}
+
+		$part1Cache[$cellId] = [];
 		foreach (yieldXY($x - 1, $y - 1, $x + 1, $y + 1) as $x2 => $y2) {
 			if ($x == $x2 && $y == $y2) { continue; }
 			if (!isset($map[$y2][$x2])) { continue; }
 
-			yield $x2 => $y2;
+			$part1Cache[$cellId][] = [$x2, $y2];
 		}
+		return $part1Cache[$cellId];
 	});
 	echo 'Part 1: ', $part1[1], ' after ', $part1[0], ' steps.', "\n";
 
-	$part2 = stepUntilNoChanges($map, 5, function($map, $x, $y) use ($minX, $minY, $maxX, $maxY) {
+	$part2Cache = [];
+	$part2 = stepUntilNoChanges($map, 5, function($map, $x, $y) use ($minX, $minY, $maxX, $maxY, &$part2Cache) {
+		$cellId = $y . '.' . $x;
+		if (isset($part2Cache[$cellId])) {
+			return $part2Cache[$cellId];
+		}
 		$changes = [[-1, 0],
 		            [0, -1],
 		            [+1, 0],
@@ -70,15 +87,18 @@
 		            [+1, +1]
 		           ];
 
+		$part2Cache[$cellId] = [];
         foreach ($changes as $c) {
 			$x2 = $x; $y2 = $y;
 			while ($x2 >= $minX && $y2 >= $minY && $x2 <= $maxX && $y2 <= $maxY) {
 				$x2 += $c[0];
 				$y2 += $c[1];
 				if (!isset($map[$y2][$x2])) { continue; }
-				yield $x2 => $y2;
+				$part2Cache[$cellId][] = [$x2, $y2];
 				break;
 			}
 		}
+		return $part2Cache[$cellId];
 	});
+
 	echo 'Part 2: ', $part2[1], ' after ', $part2[0], ' steps.', "\n";
