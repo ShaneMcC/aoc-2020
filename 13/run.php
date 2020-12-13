1aa81da9b373c9ffbcd6ca00e97c5b2b507ccc97
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-13 06:17:19 +0000

    Day 13. Part 2. 1923.

--- 13/run.php
+++ 13/run.php
@@ -5,29 +5,61 @@
 
 	$timestamp = $input[0];
 
-	$busIDs = [];
+	$busIDs = explode(',', $input[1]);
 
 	$min = $minID = 9999999;
 
-	$i = 0;
-	foreach (explode(',', $input[1]) as $b) {
-		if ($b == 'x') {
-			$c = 0;
-		} else {
-			$c = $b;
-			do {
-				$c += $b;
-			} while ($c < $timestamp);
-
-			if ($c < $min) {
-				$min = $c;
-				$minID = $b;
+	function getBusAt($busIDs, $testTime) {
+		$r = [];
+		foreach ($busIDs as $b) {
+			if ($b == 'x') { continue; }
+			if ($testTime % $b === 0) {
+				$r[] = $b;
 			}
 		}
 
-		$busIDs[$i] = [$b, $c];
-		$i++;
+		return $r;
 	}
 
-	$part1 = $minID * ($min - $timestamp);
+	$part1 = 0;
+	for ($i = $timestamp; true; $i++) {
+		$b = getBusAt($busIDs, $i);
+		if (!empty($b)) {
+			$part1 = $b[0] * ($i - $timestamp);
+			break;
+		}
+	}
+
 	echo 'Part 1: ', $part1, "\n";
+
+	$testTime = 0;
+	$lastValid = 1;
+	$lastValidID = -1;
+	while (true) {
+		if (isDebug()) {
+			echo '====', "\n";
+			echo $testTime, "\n";
+		}
+		$allTrue = true;
+		foreach ($busIDs as $k => $b) {
+			if ($b == 'x') { continue; }
+			$c = getBusAt($busIDs, $testTime + $k);
+
+			if (isDebug()) { echo "\t", ($testTime + $k), ' is ', json_encode($c), ' want ', $b, "\n"; }
+
+			if (!in_array($b, $c)) {
+				$allTrue = false;
+				break;
+			} else if ($lastValidID < $k) {
+				$lastValid *= $b;
+				$lastValidID = $k;
+			}
+		}
+
+		if ($allTrue) {
+			die('Part 2: ' . $testTime . "\n");
+		}
+
+		if (isDebug()) { echo 'Adding: ', $lastValid, "\n"; }
+		$testTime += $lastValid;
+	}
