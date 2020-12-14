df951c294392afa1ba0a212ae70db88153a3c7a6
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-14 05:27:32 +0000

    Day 14. Part 2. 427.

--- 14/run.php
+++ 14/run.php
@@ -38,9 +38,45 @@
 		}
 	}
 
-
 	$part1 = array_sum($memory);
 	echo 'Part 1: ', $part1, "\n";
 
-//	$part2 = -1;
-//	echo 'Part 2: ', $part2, "\n";
+
+	$memory = [];
+
+	foreach ($entries as $section) {
+		foreach ($section['mem'] as $mems) {
+			foreach ($mems as $mem => $val) {
+				$mem = decbin($mem);
+				$mem = str_repeat(0, 36 - strlen($mem)) . $mem;
+
+				for ($i = 0; $i < strlen($section['mask']); $i++) {
+					if ($section['mask'][$i] == '1') {
+						$mem[$i] = '1';
+					}
+				}
+
+				$possibleMems = [$mem];
+				for ($i = 0; $i < strlen($section['mask']); $i++) {
+					if ($section['mask'][$i] == 'X') {
+						$newPossible = [];
+						foreach ($possibleMems as $p) {
+							$p[$i] = '0';
+							$newPossible[] = $p;
+							$p[$i] = '1';
+							$newPossible[] = $p;
+						}
+
+						$possibleMems = $newPossible;
+					}
+				}
+
+				foreach ($possibleMems as $loc) {
+					$memory[bindec($loc)] = $val;
+				}
+			}
+		}
+	}
+
+	$part2 = array_sum($memory);
+	echo 'Part 2: ', $part2, "\n";
