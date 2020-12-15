df01ae0a4619df531e028627d7dc4bcd3b7e122e
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-15 11:54:40 +0000

    Day 15. Part 2. 13690. Bruteforce, slow.

--- 15/run.php
+++ 15/run.php
@@ -3,13 +3,12 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = explode(',', getInputLine());
 
-
 	function getSpokenNumberAt($turn, $input) {
 		$i = 1;
 		$spoken = [];
 		$lastSpoken = 0;
 		foreach ($input as $num) {
-			$spoken[$num] = [$i];
+			$spoken[$num] = [$i, $i];
 			$lastSpoken = $num;
 
 			if (isDebug()) { echo sprintf('Turn %6s', $i), ' Spoke starter number: ', $num, "\n"; }
@@ -19,20 +18,18 @@
 		while (true) {
 			if (isDebug()) { echo "\t", 'Considering: ', $lastSpoken, "\n"; }
 
-			if (count($spoken[$lastSpoken]) == 1) {
-				if (isDebug()) { echo "\t", 'Only previously spoken once.', "\n"; }
-				$num = 0;
+			$p1 = $spoken[$lastSpoken][0];
+			$p2 = $spoken[$lastSpoken][1];
+
+			if (isDebug()) { echo "\t", 'Previously spoken at ', $p1, ' and ', $p2, "\n"; }
+			$num = abs($p1 - $p2);
+
+			if (!isset($spoken[$num])) {
+				$spoken[$num] = [$i, $i];
 			} else {
-				$c = count($spoken[$lastSpoken]);
-				$p1 = $spoken[$lastSpoken][$c - 1];
-				$p2 = $spoken[$lastSpoken][$c - 2];
-
-				if (isDebug()) { echo "\t", 'Previously spoken at ', $p1, ' and ', $p2, "\n"; }
-				$num = abs($p1 - $p2);
+				$spoken[$num][0] = $spoken[$num][1];
+				$spoken[$num][1] = $i;
 			}
-
-			if (!isset($spoken[$num])) { $spoken[$num] = []; }
-			$spoken[$num][] = $i;
 			$lastSpoken = $num;
 
 			if (isDebug()) { echo sprintf('Turn %6s', $i), ' Spoke number: ', $num, "\n"; }
@@ -44,3 +41,6 @@
 
 	$part1 = getSpokenNumberAt(2020, $input);
 	echo 'Part 1: ', $part1, "\n";
+
+	$part2 = getSpokenNumberAt(30000000, $input);
+	echo 'Part 2: ', $part2, "\n";
