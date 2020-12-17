8b8cc8a12d20342bfbc0ab2ccda5ec74b41d4679
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-17 13:49:28 +0000

    Minor speed up by caching isDebug check...

--- 15/run.php
+++ 15/run.php
@@ -7,21 +7,26 @@
 		$spoken = [];
 		$num = 0;
 
+		$isDebug = isDebug();
+
 		foreach ($input as $i => $num) {
 			$spoken[$num] = ['spoken' => $i, 'diff' => 0];
-			if (isDebug()) { echo sprintf('Turn %6s', $i), "\n\t", 'Spoke starter number: ', $num, "\n"; }
+			if ($isDebug) { echo sprintf('Turn %6s', $i), "\n\t", 'Spoke starter number: ', $num, "\n"; }
 		}
 
 		for ($i = count($input); $i < $turn; $i++) {
-			$prev = $spoken[$num];
+			if ($isDebug) {
+				$prev = $spoken[$num];
 
-			if (isDebug()) {
 				echo sprintf('Turn %6s', $i), "\n";
 				echo "\t", 'Considering ', $num, ' previously spoken at ', $prev['spoken'], ($prev['diff'] > 0 ? ' and ' . ($prev['spoken'] - $prev['diff']) : ''), "\n";
 				echo "\t", 'Spoke number: ', $prev['diff'], "\n";
+
+				$num = $prev['diff'];
+			} else {
+				$num = $spoken[$num]['diff'];
 			}
 
-			$num = $prev['diff'];
 
 			if (!isset($spoken[$num])) {
 				$spoken[$num] = ['spoken' => $i, 'diff' => 0];
