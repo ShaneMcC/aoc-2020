ec9d9c86d218ac3035d29f76f27381e466ccdd20
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-18 13:55:40 +0000

    Day 18. Part 2. 10040

--- 18/run.php
+++ 18/run.php
@@ -3,7 +3,7 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLines();
 
-	function parseExpression($line) {
+	function parseExpression($line, $precedence = ['+*']) {
 		$val = 0;
 		$line = str_replace('(', ' ( ', str_replace(')', ' ) ', $line));
 		$bits = explode(' ', $line);
@@ -11,6 +11,9 @@
 		$bracketCount = 0;
 		$operator = '+';
 		$bracketed = [];
+
+		$expression = [0];
+
 		foreach ($bits as $v) {
 			if (empty($v)) { continue; }
 			if ($v == '(') {
@@ -23,7 +26,7 @@
 			} else if ($v == ')') {
 				$bracketCount--;
 				if ($bracketCount == 0) {
-					$v = parseExpression(implode(' ', $bracketed));
+					$v = parseExpression(implode(' ', $bracketed), $precedence);
 				} else {
 					$bracketed[] = $v;
 				}
@@ -35,29 +38,41 @@
 			}
 
 			if (is_numeric($v)) {
-				switch ($operator) {
-					case '+':
-						$val += $v;
-						break;
-					case '*':
-						$val *= $v;
-						break;
-					default:
-						die('Unknown Operator: ' . $operator . "\n");
-				}
+				$expression[] = $operator;
+				$expression[] = $v;
 			}
 		}
 
-		return $val;
+		$expression = implode(' ', $expression);
+		foreach ($precedence as $p) {
+			do {
+				$changed = false;
+				$expression = preg_replace_callback('/([0-9]+) ([' . preg_quote($p) . ']) ([0-9]+)/', function ($m) use (&$changed) {
+					switch ($m[2]) {
+						case '+':
+							$changed = true;
+							return $m[1] + $m[3];
+						case '*':
+							$changed = true;
+							return $m[1] * $m[3];
+					}
+
+					die('Unable to handle expression.');
+				}, $expression, 1);
+			} while ($changed);
+		}
+
+		return $expression;
 	}
+
 
 	$entries = [];
 	foreach ($input as $line) {
-		$entries[] = [$line, parseExpression($line)];
+		$entries[] = [$line, parseExpression($line, ['+*']), parseExpression($line, ['+', '*'])];
 	}
 
 	$part1 = array_sum(array_column($entries, 1));
 	echo 'Part 1: ', $part1, "\n";
 
-	/* $part2 = -1;
-	echo 'Part 2: ', $part2, "\n"; */
+	$part2 = array_sum(array_column($entries, 2));
+	echo 'Part 2: ', $part2, "\n";
