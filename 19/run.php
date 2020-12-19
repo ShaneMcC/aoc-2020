3cda9df3f55901a74c9c96538b823a9b2acd63eb
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-19 14:52:09 +0000

    Day 19. Part 2. 5907.

--- 19/run.php
+++ 19/run.php
@@ -4,18 +4,28 @@
 	$input = getInputLineGroups();
 
 	$rules = [];
-	foreach ($input[0] as $rule) {
-		$bits = explode(': ', $rule, 2);
 
-		$r = ['str' => $bits[1], 'rules' => []];
+	function createRule($ruleString) {
+		$bits = explode(': ', $ruleString, 2);
+
+		$rule = ['str' => $bits[1], 'rules' => []];
 		foreach (explode(' | ', $bits[1]) as $rulebit) {
-			$r['rules'][] = trim($rulebit);
+			$rule['rules'][] = trim($rulebit);
 		}
 
-		$rules[$bits[0]] = $r;
+		return [$bits[0], $rule];
 	}
 
-	function buildRegex($rules, $ruleId) {
+	foreach ($input[0] as $rule) {
+		[$rid, $r] = createRule($rule);
+		$rules[$rid] = $r;
+	}
+
+	function buildRegex($rules, $ruleId, $overrides = []) {
+		if (isset($overrides[$ruleId])) {
+			return $overrides[$ruleId];
+		}
+
 		$ruleinfo = $rules[$ruleId]['rules'];
 
 		$options = [];
@@ -25,7 +35,7 @@
 			} else {
 				$ruleOptions = [];
 				foreach (explode(' ', $rule) as $rid) {
-					$ruleOptions[] = buildRegex($rules, $rid);
+					$ruleOptions[] = buildRegex($rules, $rid, $overrides);
 				}
 				$options[] = (count($ruleOptions) == 1) ? $ruleOptions[0] : implode($ruleOptions);
 			}
@@ -33,7 +43,6 @@
 
 		return (count($options) == 1) ? $options[0] : '(' . implode('|', $options) . ')';
 	}
-
 
 	$part1regex = '#^' . buildRegex($rules, 0) . '$#';
 
@@ -45,5 +54,28 @@
 	}
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	$overrides = [];
+	$maxLen = 0;
+	foreach ($input[1] as $message) { $maxLen = max($maxLen, strlen($message)); }
+
+	// $rules[8] = createRule('8: 42 | 42 8')[1];
+	$overrides[8] = '(' . buildRegex($rules, 42) . ')+';
+
+	// $rules[11] = createRule('11: 42 31 | 42 11 31')[1];
+	$overrides[11] = [];
+	$def = '(?(DEFINE)(?<r42>' . buildRegex($rules, 42) . ')(?<r31>' . buildRegex($rules, 31) . '))';
+	for ($i = 1; $i < ($maxLen / 2); $i++) {
+		$repeat = '{' . $i . '}';
+		$overrides[11][] = '(?&r42)' . $repeat . '(?&r31)' . $repeat;
+	}
+	$overrides[11] = $def . '(' . implode('|', $overrides[11]) . ')';
+
+	$part2regex = '#^' . buildRegex($rules, 0, $overrides) . '$#';
+
+	$part2 = 0;
+	foreach ($input[1] as $message) {
+		if (preg_match($part2regex, $message)) {
+			$part2++;
+		}
+	}
+	echo 'Part 2: ', $part2, "\n";
