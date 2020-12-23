ef33796ae6a1d3abc3f6be8051d6a75a111ffe06
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-23 18:30:19 +0000

    Day 23. Part 2. 6981.

--- 23/run.php
+++ 23/run.php
@@ -3,70 +3,125 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLine();
 
-	$cups = str_split($input);
+	class Cup {
+		public $val = 0;
+
+		public $next = null;
+		public $prev = null;
+
+		public function __construct($val) {
+			$this->val = $val;
+		}
+	}
+
+	function getCups($input, $totalNeeded = 0) {
+		$first = NULL;
+		$prev = NULL;
+
+		$cups = [];
+
+		foreach (str_split($input) as $c) {
+			$cup = new Cup($c);
+			$cups[$c] = $cup;
+
+			$cup->prev = $prev;
+			if ($prev == null) { $first = $cup; }
+			else { $prev->next = $cup; }
+			$prev = $cup;
+		}
+
+		for ($i = count($cups) + 1; $i <= $totalNeeded; $i++) {
+			$cup = new Cup($i);
+			$cups[$i] = $cup;
+
+			$cup->prev = $prev;
+			if ($prev == null) { $first = $cup; }
+			else { $prev->next = $cup; }
+			$prev = $cup;
+		}
+
+		$first->prev = $prev;
+		$prev->next = $first;
+
+		return $cups;
+	}
 
 	function moveCups($cups, $moves) {
 		$isDebug = isDebug();
 
-		$currentIndex = 0;
+		$minCup = min(array_keys($cups));
+		$maxCup = max(array_keys($cups));
 
-		$minCup = min($cups);
-		$maxCup = max($cups);
+		$currentCup = $cups[array_keys($cups)[0]];
 
 		for ($move = 1; $move <= $moves; $move++) {
-			// Current Cup Label
-			$currentLabel = $cups[$currentIndex];
 			if ($isDebug) {
 				echo '-- move ', $move, ' --', "\n";
 				echo 'cups:';
-				for ($c = 0; $c < count($cups); $c++) {
-					echo ($c == $currentIndex) ? ' (' : '  ';
-					echo $cups[$c];
-					echo ($c == $currentIndex) ? ')' : '';
+				$indexCup = $currentCup->prev->prev;
+				for ($c = 0; $c < 14; $c++) {
+					echo ($indexCup == $currentCup) ? ' (' : '  ';
+					echo $indexCup->val;
+					echo ($indexCup == $currentCup) ? ')' : ' ';
+					$indexCup = $indexCup->next;
 				}
 				echo "\n";
 			};
 
-			$pickup = array_splice($cups, $currentIndex + 1, 3, []);
-			if (count($pickup) < 3) {
-				$pickup = array_merge($pickup, array_splice($cups, 0, 3 - count($pickup), []));
-			}
+			// Pickup
+			$pickupStart = $currentCup->next;
+			$pickupEnd = $pickupStart->next->next;
 
-			$destination = $currentLabel;
+			// Remove from circle
+			$currentCup->next = $pickupEnd->next;
+			$pickupEnd->prev = $currentCup;
+
+
+			// Icky.
+			$pickupLabels = [$pickupStart->val, $pickupStart->next->val, $pickupStart->next->next->val];
+			$destination = $currentCup->val;
 			do {
 				$destination -= 1;
 				if ($destination < $minCup) { $destination = $maxCup; }
-			} while (in_array($destination, $pickup));
+			} while (in_array($destination, $pickupLabels));
 
 			if ($isDebug) {
-				echo 'pick up: ', implode(', ', $pickup), "\n";
+				echo 'pick up: ', implode(', ', $pickupLabels), "\n";
 				echo 'destination: ', $destination, "\n";
 				echo "\n";
 			}
 
 			// Insert Cups
-			$index = array_search($destination, $cups);
-			array_splice($cups, $index, 1, array_merge([$destination], $pickup));
+			$destCup = $cups[$destination];
+			$afterCup = $destCup->next;
+
+			// Splice into circle.
+			$destCup->next = $pickupStart;
+			$pickupStart->prev = $destCup;
+			$pickupEnd->next = $afterCup;
+			$afterCup->prev = $pickupEnd;
 
 			// Move Current Cup
-			$currentIndex = (array_search($currentLabel, $cups) + 1) % count($cups);
+			$currentCup = $currentCup->next;
 		}
 
 		return $cups;
 	}
 
-	function getCupsAsAnswer($cups) {
-		$end = array_splice($cups, array_search('1', $cups));
-		$final = array_merge($end, $cups);
-		array_shift($final);
-
-		return implode('', $final);
+	$part1Cups = getCups($input, 0);
+	$part1Cups = moveCups($part1Cups, 100);
+	$c = $part1Cups[1]->next;
+	$part1 = '';
+	while ($c->val != '1') {
+		$part1 .= $c->val;
+		$c = $c->next;
 	}
-
-	$part1 = moveCups($cups, 100);
-	$part1 = getCupsAsAnswer($part1);
-
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2Cups = getCups($input, 1000000);
+	$part2Cups = moveCups($part2Cups, 10000000);
+
+	$first = $part2Cups[1]->next;
+	$second = $part2Cups[1]->next->next;
+	$part2 = $first->val * $second->val;
+	echo 'Part 2: ', $part2, "\n";
