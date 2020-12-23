8d0503d1e575e5b8a5c36195d6adcbd5e0578d5d
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-23 19:20:20 +0000

    Minor tidying.

--- 23/run.php
+++ 23/run.php
@@ -6,14 +6,10 @@
 	function getCups($input, $totalNeeded = 0) {
 		$cups = [];
 		$prev = NULL;
-		foreach (str_split($input) as $c) {
-			$c = intval($c);
-			$cups[$c] = NULL;
-			if ($prev != NULL) { $cups[$prev] = $c; }
-			$prev = $c;
-		}
 
-		for ($c = count($cups) + 1; $c <= $totalNeeded; $c++) {
+		for ($i = 1; $i <= max(strlen($input), $totalNeeded); $i++) {
+			$c = ($i <= strlen($input)) ? intval($input[$i - 1]) : $i;
+
 			$cups[$c] = NULL;
 			if ($prev != NULL) { $cups[$prev] = $c; }
 			$prev = $c;
@@ -27,10 +23,10 @@
 	function moveCups($cups, $moves) {
 		$isDebug = isDebug();
 
-		$minCup = min(array_keys($cups));
-		$maxCup = max(array_keys($cups));
-
-		$currentCup = array_keys($cups)[0];
+		$keys = array_keys($cups);
+		$minCup = min($keys);
+		$maxCup = max($keys);
+		$currentCup = $keys[0];
 
 		for ($move = 1; $move <= $moves; $move++) {
 			if ($isDebug) {
@@ -48,24 +44,19 @@
 
 			// Pickup
 			$pickupStart = $cups[$currentCup];
-			$pickupEnd = $cups[$cups[$pickupStart]];
+			$pickupMid = $cups[$pickupStart];
+			$pickupEnd = $cups[$pickupMid];
 
 			// Remove from circle
 			$cups[$currentCup] = $cups[$pickupEnd];
 
-			// Icky.
-			$pickupLabels = [$pickupStart, $cups[$pickupStart], $cups[$cups[$pickupStart]]];
+			// Find Destination
+			$pickupLabels = [$pickupStart, $pickupMid, $pickupEnd];
 			$destination = $currentCup;
 			do {
 				$destination -= 1;
 				if ($destination < $minCup) { $destination = $maxCup; }
 			} while (in_array($destination, $pickupLabels));
-
-			if ($isDebug) {
-				echo 'pick up: ', implode(', ', $pickupLabels), "\n";
-				echo 'destination: ', $destination, "\n";
-				echo "\n";
-			}
 
 			// Insert Cups
 			$destCup = $destination;
@@ -77,25 +68,26 @@
 
 			// Move Current Cup
 			$currentCup = $cups[$currentCup];
+
+			if ($isDebug) {
+				echo 'pick up: ', implode(', ', $pickupLabels), "\n";
+				echo 'destination: ', $destination, "\n";
+				echo "\n";
+			}
 		}
 
 		return $cups;
 	}
 
-	$part1Cups = getCups($input, 0);
-	$part1Cups = moveCups($part1Cups, 100);
-	$c = $part1Cups[1];
+	$part1Cups = moveCups(getCups($input, 0), 100);
 	$part1 = '';
+	$c = $part1Cups[1];
 	while ($c != '1') {
 		$part1 .= $c;
 		$c = $part1Cups[$c];
 	}
 	echo 'Part 1: ', $part1, "\n";
 
-	$part2Cups = getCups($input, 1000000);
-	$part2Cups = moveCups($part2Cups, 10000000);
-
-	$first = $part2Cups[1];
-	$second = $part2Cups[$first];
-	$part2 = $first * $second;
+	$part2Cups = moveCups(getCups($input, 1000000), 10000000);
+	$part2 = $part2Cups[1] * $part2Cups[$part2Cups[1]];
 	echo 'Part 2: ', $part2, "\n";
