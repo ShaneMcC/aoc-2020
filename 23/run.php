5b1bd59fe233129ceab9ffea2b96763100d1a4af
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-23 19:09:08 +0000

    Speed up by not using objects.
    Because we only ever care about the "next" cup in the list, we can
    actually just keep a 1000000 long array (which we were doing anyway),
    and rather than storing an object with a ->next pointer, just store
    the next index instead directly.

--- 23/run.php
+++ 23/run.php
@@ -3,33 +3,23 @@
 	require_once(dirname(__FILE__) . '/../common/common.php');
 	$input = getInputLine();
 
-	class Cup {
-		public $val = 0;
-		public $next = null;
-
-		public function __construct($val) {
-			$this->val = $val;
-		}
-	}
-
 	function getCups($input, $totalNeeded = 0) {
 		$cups = [];
 		$prev = NULL;
 		foreach (str_split($input) as $c) {
-			$cup = new Cup($c);
-			$cups[$c] = $cup;
-			if ($prev != null) { $prev->next = $cup; }
-			$prev = $cup;
+			$c = intval($c);
+			$cups[$c] = NULL;
+			if ($prev != NULL) { $cups[$prev] = $c; }
+			$prev = $c;
 		}
 
 		for ($c = count($cups) + 1; $c <= $totalNeeded; $c++) {
-			$cup = new Cup($c);
-			$cups[$c] = $cup;
-			if ($prev != null) { $prev->next = $cup; }
-			$prev = $cup;
+			$cups[$c] = NULL;
+			if ($prev != NULL) { $cups[$prev] = $c; }
+			$prev = $c;
 		}
 
-		$prev->next = $cups[array_keys($cups)[0]];
+		$cups[$prev] = array_keys($cups)[0];
 
 		return $cups;
 	}
@@ -40,7 +30,7 @@
 		$minCup = min(array_keys($cups));
 		$maxCup = max(array_keys($cups));
 
-		$currentCup = $cups[array_keys($cups)[0]];
+		$currentCup = array_keys($cups)[0];
 
 		for ($move = 1; $move <= $moves; $move++) {
 			if ($isDebug) {
@@ -49,23 +39,23 @@
 				$indexCup = $currentCup;
 				for ($c = 0; $c < min(count($cups), 14); $c++) {
 					echo ($indexCup == $currentCup) ? ' (' : '  ';
-					echo $indexCup->val;
+					echo $indexCup;
 					echo ($indexCup == $currentCup) ? ')' : ' ';
-					$indexCup = $indexCup->next;
+					$indexCup = $cups[$indexCup];
 				}
 				echo "\n";
 			};
 
 			// Pickup
-			$pickupStart = $currentCup->next;
-			$pickupEnd = $pickupStart->next->next;
+			$pickupStart = $cups[$currentCup];
+			$pickupEnd = $cups[$cups[$pickupStart]];
 
 			// Remove from circle
-			$currentCup->next = $pickupEnd->next;
+			$cups[$currentCup] = $cups[$pickupEnd];
 
 			// Icky.
-			$pickupLabels = [$pickupStart->val, $pickupStart->next->val, $pickupStart->next->next->val];
-			$destination = $currentCup->val;
+			$pickupLabels = [$pickupStart, $cups[$pickupStart], $cups[$cups[$pickupStart]]];
+			$destination = $currentCup;
 			do {
 				$destination -= 1;
 				if ($destination < $minCup) { $destination = $maxCup; }
@@ -78,15 +68,15 @@
 			}
 
 			// Insert Cups
-			$destCup = $cups[$destination];
-			$afterCup = $destCup->next;
+			$destCup = $destination;
+			$afterCup = $cups[$destCup];
 
 			// Splice into circle.
-			$destCup->next = $pickupStart;
-			$pickupEnd->next = $afterCup;
+			$cups[$destCup] = $pickupStart;
+			$cups[$pickupEnd] = $afterCup;
 
 			// Move Current Cup
-			$currentCup = $currentCup->next;
+			$currentCup = $cups[$currentCup];
 		}
 
 		return $cups;
@@ -94,18 +84,18 @@
 
 	$part1Cups = getCups($input, 0);
 	$part1Cups = moveCups($part1Cups, 100);
-	$c = $part1Cups[1]->next;
+	$c = $part1Cups[1];
 	$part1 = '';
-	while ($c->val != '1') {
-		$part1 .= $c->val;
-		$c = $c->next;
+	while ($c != '1') {
+		$part1 .= $c;
+		$c = $part1Cups[$c];
 	}
 	echo 'Part 1: ', $part1, "\n";
 
 	$part2Cups = getCups($input, 1000000);
 	$part2Cups = moveCups($part2Cups, 10000000);
 
-	$first = $part2Cups[1]->next;
-	$second = $part2Cups[1]->next->next;
-	$part2 = $first->val * $second->val;
+	$first = $part2Cups[1];
+	$second = $part2Cups[$first];
+	$part2 = $first * $second;
 	echo 'Part 2: ', $part2, "\n";
