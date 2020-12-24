2c95b6f71bb1d7094141b07c727a0b3628d939e5
Shane Mc Cormack <shanemcc@gmail.com>
2020-12-24 11:39:41 +0000

    Day 24. Part 2. 6269

--- 24/run.php
+++ 24/run.php
@@ -5,40 +5,36 @@
 
 	function getTile($directions) {
 		// Hex Grids: https://www.redblobgames.com/grids/hexagons/
-		$x = $y = $z = 0;
+		$r = $q = 0;
 
 		// Pointy-Topped Hexes.
 		// https://www.redblobgames.com/grids/hexagons/#coordinates-cube
 		foreach ($directions as $dir) {
 			switch ($dir) {
 				case 'nw':
-					$y++;
-					$z--;
+					$r--;
 					break;
 				case 'ne':
-					$x++;
-					$z--;
+					$q++;
+					$r--;
 					break;
 				case 'sw':
-					$x--;
-					$z++;
+					$q--;
+					$r++;
 					break;
 				case 'se':
-					$z++;
-					$y--;
+					$r++;
 					break;
 				case 'w':
-					$x--;
-					$y++;
+					$q--;
 					break;
 				case 'e':
-					$x++;
-					$y--;
+					$q++;
 					break;
 			}
 		}
 
-		return [$x, $y, $z];
+		return [$r, $q];
 	}
 
 	$map = [];
@@ -52,17 +48,66 @@
 			$directions[] = $move;
 		}
 
-		[$x, $y, $z] = getTile($directions);
+		[$r, $q] = getTile($directions);
 
-		$id = $z . '.' . $y . '.' . $x;
-		if (!isset($map[$id])) { $map[$id] = 0; }
+		if (!isset($map[$r])) { $map[$r] = []; }
+		if (!isset($map[$r][$q])) { $map[$r][$q] = 0; }
 
-		echo $line, ' flips ', $id, ' from ', $map[$id], "\n";
-
-		$map[$id] = ($map[$id] === 0) ? 1 : 0;
+		$map[$r][$q] = ($map[$r][$q] === 0) ? 1 : 0;
 	}
 
-	echo 'Part 1: ', array_count_values($map)[1], "\n";
+	$part1 = 0;
+	foreach ($map as $r => $qR) {
+		foreach ($qR as $q => $c) {
+			if ($c === 1) { $part1++; }
+		}
+	}
 
-	// $part2 = -1;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 1: ', $part1, "\n";
+
+	for ($day = 1; $day <= 100; $day++) {
+		$newMap = $map;
+
+		[$minQ, $minR, $maxQ, $maxR] = getBoundingBox($map);
+
+		for ($r = $minR - 2; $r < $maxR + 2; $r++) {
+			for ($q = $minQ - 2; $q < $maxQ + 2; $q++) {
+
+				$adjacent = [[$r - 1, $q],
+				             [$r - 1, $q + 1],
+				             [$r + 1, $q - 1],
+				             [$r + 1, $q],
+				             [$r, $q - 1],
+				             [$r, $q + 1],
+				            ];
+
+				$ac = 0;
+				foreach ($adjacent as $t) {
+					[$tR, $tQ] = $t;
+
+					if (isset($map[$tR][$tQ]) && $map[$tR][$tQ] === 1) {
+						$ac++;
+					}
+				}
+
+				if (isset($map[$r][$q]) && $map[$r][$q] == 1) {
+					if ($ac == 0 || $ac > 2) {
+						$newMap[$r][$q] = 0;
+					}
+				} else {
+					if ($ac == 2) {
+						$newMap[$r][$q] = 1;
+					}
+				}
+			}
+		}
+		$map = $newMap;
+	}
+
+	$part2 = 0;
+	foreach ($newMap as $r => $qR) {
+		foreach ($qR as $q => $c) {
+			if ($c === 1) { $part2++; }
+		}
+	}
+	echo 'Part 2: ', $part2, "\n";
